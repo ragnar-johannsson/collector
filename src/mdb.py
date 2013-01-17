36cecae0d4e08452bc1952db510bf52ed2457c5a
Ragnar B. Johannsson <ragnar@greenqloud.com>
2013-01-17 14:45:42 +0000

    Improved MongoDB connection handling.

--- src/mdb.py
+++ src/mdb.py
@@ -1,9 +1,24 @@
+import logging
+
 from json import loads as jsonify
 from pymongo import MongoClient
 
+connections = dict()
+
+def initdb(resources):
+    for config in resources:
+        if 'mongodb_user' in config:
+            url = 'mongodb://%s:%s@%s' % (config['mongodb_user'], config['mongodb_passwd'], config['mongodb_host'])
+        else:
+            url = 'mongodb://%s' % config['mongodb_host']
+        if 'mongodb_port' in config:
+            url += ":%s" % config['mongodb_port']
+
+        logging.debug('Connecting to %s' % url)
+        connections[config['name']] = MongoClient(url, max_pool_size=20)
+
 def persist(data, config):
-    conn = MongoClient(config['mongodb_host'], config['mongodb_port'])
-    db = conn[config['mongodb_db']]
+    db = connections[config['name']][config['mongodb_db']]
     collection = db[config['mongodb_collection']]
 
     collection.insert(jsonify(data))
--- src/server.py
+++ src/server.py
@@ -2,13 +2,17 @@ import logging
 
 from config import host, port, resources
 from flask import Flask, abort, request
+from mdb import persist, initdb
 from signature import verify_signature
-from mdb import persist
 
 app = Flask(__name__)
 
 @app.route("/<resource>", methods=['POST'])
 def serve(resource):
+    logging.debug('Connection from: %s' % request.remote_addr)
+    logging.debug('Resource: %s' % resource)
+    logging.debug('Query string %s' % request.query_string)
+
     found = filter(lambda x: x['name'] == resource, resources)
     if not found: 
         abort(404) 
@@ -19,13 +23,14 @@ def serve(resource):
 
     try:
         for k in request.form.iterkeys():
+            logging.debug('Request data: %s' % k)
             persist(k, resource)
     except Exception as e:
-        logging.error('Error persisting data: %s' % e.message)
-        logging.debug('Data: %s' % k)
+        logging.error('Error persisting data: %s' % str(e))
         return "Error saving data", 500
 
     return "OK"
 
 if __name__ == "__main__":
+    initdb(resources)
     app.run(debug=True, host=host, port=port)
